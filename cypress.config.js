aabe5f68eac395958226f0e176000981a2bfe50c
sercanxs <sercanxssahin@hotmail.com>
2023-06-13 12:02:09 +0300

    cypress

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://practice.cydeo.com/',
+    env: {
+      login:"/login",
+      apiUrl:"https://demoqa.com",
+      apiBooks:"/BookStore/v1/Books",
+      generateUser:"/Account/v1/User",
+      generateToken:"/Account/v1/GenerateToken",
+      loginAPI:"/Account/v1/Login"
+    },
+    video: false,
+    retries: 1,
+    defaultCommandTimeout: 5000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
