fcde2eb8de82e2921c6372f4a467c9a0e739930a
sercanxs <sercanxssahin@hotmail.com>
2023-06-13 12:53:34 +0300

    test

--- cypress/e2e/apiTests/spartanApi.cy.js
+++ cypress/e2e/apiTests/spartanApi.cy.js
@@ -1,12 +1,12 @@
 describe('Spartan API tests', { baseUrl: 'http://54.226.211.37:8000/' }, () => {
-  it('Get a single spartan', () => {
+  it.skip('Get a single spartan', () => {
     cy.request('GET', 'api/spartans/100').then((response) => {
       expect(response.status).to.equal(200);
       expect(response.body.name).to.equal('Terence');
     });
   });
 
-  it('POST one spartan test', () => {
+  it.skip('POST one spartan test', () => {
     cy.request({
       method: 'POST',
       url: 'api/spartans',
